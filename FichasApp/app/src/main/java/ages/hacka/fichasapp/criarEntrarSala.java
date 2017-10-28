d1aeb32146232c7ebeb8b841ae36cd2a58869a24
mazzardo <bruno.mazzardo@acad.pucrs.br>
2017-10-28 15:39:39 -0200

    Arrumando tela de criar e entrar e levando para tela de criação

--- FichasApp/app/src/main/java/ages/hacka/fichasapp/criarEntrarSala.java
+++ FichasApp/app/src/main/java/ages/hacka/fichasapp/criarEntrarSala.java
@@ -1,17 +1,37 @@
 package ages.hacka.fichasapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
 
 
 public class criarEntrarSala extends AppCompatActivity {
+
+    Button btnCriarSala;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         this.setContentView(R.layout.criar_entrar_sala);
+
+
+        btnCriarSala= (Button) findViewById(R.id.criar_sala_btn);
+        btnCriarSala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(),configuracaoSalaActivity.class);
+                startActivity(intent);
+                finish();
+
+
+
+            }
+        });
     }
 }
