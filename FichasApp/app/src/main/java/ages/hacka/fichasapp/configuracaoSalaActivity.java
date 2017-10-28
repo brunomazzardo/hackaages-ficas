29ca560c23fce81acf3819b89cd39c0fc71a2dd4
root <vitor.edgar@acad.pucrs.br>
2017-10-28 16:12:58 -0200

    Tela de de configuração de sala update

--- FichasApp/app/src/main/java/ages/hacka/fichasapp/configuracaoSalaActivity.java
+++ FichasApp/app/src/main/java/ages/hacka/fichasapp/configuracaoSalaActivity.java
@@ -8,6 +8,7 @@ public class configuracaoSalaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_configuracao_sala);
     }
 }
