ae62d9d49010c0e5dfa2573ec1c629fe26749441
mazzardo <bruno.mazzardo@acad.pucrs.br>
2017-10-29 06:47:39 -0200

    Desespero

--- FichasApp/app/src/main/java/ages/hacka/fichasapp/util/AdicionaAposta.java
+++ FichasApp/app/src/main/java/ages/hacka/fichasapp/util/AdicionaAposta.java
@@ -1,13 +1,18 @@
 package ages.hacka.fichasapp.util;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 import ages.hacka.fichasapp.model.Jogada;
+import ages.hacka.fichasapp.model.Sala;
 
 /**
  * Created by mazzardo on 29/10/17.
@@ -16,16 +21,9 @@ import ages.hacka.fichasapp.model.Jogada;
 public class AdicionaAposta {
 
     public static boolean adiciona(final Jogada jogada){
-
-        Random random = new Random();
-        int key= random.nextInt(99999)+1;
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference ref = rootRef.child("salas/-KxaHQa9r_igFHHhMy01/jogos/0/mao/");
-//        String key =ref.child("jogadas").push().getKey();
-        Map<String, Object> childUpdates = new HashMap<>();
-        childUpdates.put("/jogadas/"+key, jogada);
-        ref.updateChildren(childUpdates);
-
+        final ArrayList<Jogada> jogadas = new ArrayList<>();
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("salas/-KxaHQa9r_igFHHhMy01/jogos/0/mao/jogadas/");
+        databaseReference.setValue(jogada);
 
         return false;
 
