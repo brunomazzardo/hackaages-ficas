61e079ff4f1cd64c2fe248c613e257ec9d15603b
root <vitor.edgar@acad.pucrs.br>
2017-10-29 01:07:04 -0200

    Ajustes no layout da tela de jogo

--- FichasApp/app/src/main/java/ages/hacka/fichasapp/ConfiguracaoSalaActivity.java
+++ FichasApp/app/src/main/java/ages/hacka/fichasapp/ConfiguracaoSalaActivity.java
@@ -1,14 +1,32 @@
 package ages.hacka.fichasapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ages.hacka.fichasapp.activities.TestFirebaseActivity;
 
 public class ConfiguracaoSalaActivity extends AppCompatActivity {
+
+    Button btnCriarSala;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_configuracao_sala);
+
+        btnCriarSala = findViewById(R.id.criarSalaBtn);
+
+        btnCriarSala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(),SalaJogoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
